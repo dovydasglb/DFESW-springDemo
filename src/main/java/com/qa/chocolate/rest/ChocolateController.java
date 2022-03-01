8961ed77ecc3f7cbf9deace4e534749198c81ed1
dovydasglb <dovydasglb@gmail.com>
2022-03-01 21:37:42 +0000

    Chocolate controller created including response entity example

--- src/main/java/com/qa/chocolate/rest/ChocolateController.java
+++ src/main/java/com/qa/chocolate/rest/ChocolateController.java
@@ -3,10 +3,13 @@ package com.qa.chocolate.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,35 +19,54 @@ import com.qa.chocolate.domain.Chocolate;
 public class ChocolateController {
 
 	private List<Chocolate> choco = new ArrayList<>();
-	
-	//CRUD
-	
-	//create - POST
+
+	// response entity -> allows us to configure the status code of the response
+	// (example in POST below)
+
+	// CRUD
+
+	// create - POST
+	// Response to return instead of the default 200: 201 - created
 	@PostMapping("/createChoco")
-	public void createChoco(@RequestBody Chocolate c) {
-		
-		//service -> create
+	public ResponseEntity<Chocolate> createChoco(@RequestBody Chocolate c) {
+		// service -> create
 		this.choco.add(c);
+		Chocolate newChoco = this.choco.get(this.choco.size() - 1);// could also write this directly within response
+																	// entity brackets below instead of creating the
+																	// object
+		return new ResponseEntity<Chocolate>(newChoco, HttpStatus.CREATED);
 	}
-	
-	//insert into chocolate(name, brand, ...) values("dairy milk", "cadbury", ...)
-	
-	//read - GET
+
+	// insert into chocolate(name, brand, ...) values("dairy milk", "cadbury", ...)
+
+	// read - GET
 	@GetMapping("/getChoco")
 	public List<Chocolate> getChoco() {
 		return this.choco;
 	}
-	
-	//read by ID - GET
+
+	// read by ID - GET
 	@GetMapping("/getOne/{id}")
 	public Chocolate getOne(@PathVariable int id) {
 		return this.choco.get(id);
 	}
-	
-	
-	//update - PUT/PATCH
-	
-	//delete - DELETE
+
+	// update - PUT/PATCH
+	// create, read by id
+	@PutMapping("/updateChoco/{id}")
+	public Chocolate updateChoco(@PathVariable int id, @RequestBody Chocolate ch) {
+		// remove existing choco with id
+		// this.choco.remove(id);
+		// add a new choco in its place
+		// this.choco.add(id, ch);
+
+		// combines remove and add which are commented out above
+		this.choco.set(id, ch);
+		// return updated choco
+		return this.choco.get(id);
+	}
+
+	// delete - DELETE
 	@DeleteMapping("/removeChoco/{id}")
 	public Chocolate removeChoco(@PathVariable int id) {
 		return this.choco.remove(id);
